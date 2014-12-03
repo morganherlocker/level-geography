6b34d23179d4e2ac9caba7c99d27ecb8c3c5f69a
morganherlocker <morgan.herlocker@gmail.com>
2014-12-03 15:04:25 -0500

    sublevel 1st pass :S

--- index.js
+++ index.js
@@ -1,49 +1,54 @@
 var cover = require('tile-cover'),
     getCoords = require('geojson-coords');
 
-module.exports.put = function(db, feature, featureId, done){
-    var indexes = cover.indexes(feature.geometry, {min_zoom: 3, max_zoom: 9});
-    var items = indexes.map(function(index){
-        return {
-            type: 'put',
-            key: index+'!'+featureId,
-            value: JSON.stringify(feature)
+module.exports = function (db) {
+    db.geoPut = function geoPut(feature, featureId, done) {
+        var indexes = cover.indexes(feature.geometry, {min_zoom: 3, max_zoom: 9});
+        var items = indexes.map(function(index){
+            return {
+                type: 'put',
+                key: index+'!'+featureId,
+                value: JSON.stringify(feature)
+            };
+        });
+        db.batch(items, function(err){
+            done(err);
+        });
+    };
+
+    db.bboxQuery = function(bbox, done) {
+        var fc = {
+            type: 'FeatureCollection',
+            features: []
         };
-    });
-    db.batch(items, function(err){
-        done(err);
-    });
-};
+        var featureIds = [];
+        var lowGeometry = {
+            type: 'Point',
+            coordinates: [bbox[0], bbox[3]]
+        };
+        var highGeometry = {
+            type: 'Point',
+            coordinates: [bbox[2], bbox[1]]
+        };
+        var lowIndex = cover.indexes(lowGeometry, {min_zoom: 3, max_zoom: 20})[0];
+        var highIndex = cover.indexes(highGeometry, {min_zoom: 3, max_zoom: 20})[0];
 
-module.exports.bboxQuery = function(db, bbox, done){
-    var fc = {
-        type: 'FeatureCollection',
-        features: []
+        db.createReadStream({gte: lowIndex, lte: highIndex})
+        .on('data', function (data) {
+            var featureId = data.key.split('!')[data.key.split('!').length - 1]
+            if(featureIds.indexOf(featureId) === -1) {
+                featureIds.push(featureId)
+                fc.features.push(JSON.parse(data.value));
+            }
+        })
+        .on('error', function (err) {
+            done(err);
+        })
+        .on('end', function () {
+            done(null, fc);
+        });
     };
-    var featureIds = [];
-    var lowGeometry = {
-        type: 'Point',
-        coordinates: [bbox[0], bbox[3]]
-    };
-    var highGeometry = {
-        type: 'Point',
-        coordinates: [bbox[2], bbox[1]]
-    };
-    var lowIndex = cover.indexes(lowGeometry, {min_zoom: 3, max_zoom: 20})[0];
-    var highIndex = cover.indexes(highGeometry, {min_zoom: 3, max_zoom: 20})[0];
 
-    db.createReadStream({gte: lowIndex, lte: highIndex})
-    .on('data', function (data) {
-        var featureId = data.key.split('!')[data.key.split('!').length - 1]
-        if(featureIds.indexOf(featureId) === -1) {
-            featureIds.push(featureId)
-            fc.features.push(JSON.parse(data.value));
-        }
-    })
-    .on('error', function (err) {
-        done(err);
-    })
-    .on('end', function () {
-        done(null, fc);
-    });
+
+    return db;
 };
--- test.js
+++ test.js
@@ -1,17 +1,18 @@
-var test = require('tape'),
-    levelup = require('levelup'),
-    leveldown = require('leveldown'),
-    fs = require('fs'),
-    queue = require('queue-async'),
-    geo = require('./');
+var test = require('tape');
+var fs = require('fs');
+var queue = require('queue-async');
+var level = require('level');
+var sublevel = require('level-sublevel');
+var levelGeo = require('./');
+var db = levelGeo(sublevel(level('./db')));
 
 test('insert, query', function(t){
     var pts = JSON.parse(fs.readFileSync('./fixtures/points.geojson'));
-    var db = levelup('./db');
+    var db = levelGeo(sublevel(level('./db')));
     var q = queue(1);
 
     pts.features.forEach(function(pt, i){
-        q.defer(geo.put, db, pt, i.toString());
+        q.defer(db.geoPut, pt, i.toString());
     });
 
     q.awaitAll(function(err){
@@ -25,7 +26,7 @@ test('insert, query', function(t){
             t.notOk(err);
         })
         .on('end', function () {
-            geo.bboxQuery(db, [ -107.841796875,34.161818161230386,-103.5791015625,37.16031654673677], function(err, fc){
+            db.bboxQuery([ -107.841796875,34.161818161230386,-103.5791015625,37.16031654673677], function(err, fc){
                 t.notOk(err, 'bbox query');
                 t.notEqual(fc.features.length, 0);
             });
@@ -42,11 +43,11 @@ test('insert, query', function(t){
 
 test('insert polygon -- verify dedupe', function(t){
     var poly = JSON.parse(fs.readFileSync('./fixtures/polygon.geojson'));
-    var db = levelup('./db');
+    var db = levelGeo(sublevel(level('./db')));
 
-    geo.put(db, poly, '1', function(err){
+    db.geoPut(poly, '1', function(err){
         t.notOk(err, 'polygon inserted');
-        geo.bboxQuery(db, [ 21.9287109375,12.382928338487408,35.5078125,25.720735134412106], function(err, fc){
+        db.bboxQuery([ 21.9287109375,12.382928338487408,35.5078125,25.720735134412106], function(err, fc){
             t.notOk(err, 'bbox query');
             t.equal(fc.features.length, 1);
             db.close(function(err){
